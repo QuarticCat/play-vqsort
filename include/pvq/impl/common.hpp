3361b438a0d37b06647198dbff239c2a925340cf
QuarticCat <QuarticCat@protonmail.com>
2022-06-18 10:39:43 -0700

    add utils

--- include/pvq/impl/common.hpp
+++ include/pvq/impl/common.hpp
@@ -2,15 +2,19 @@
 
 #include <immintrin.h>
 
+#include <algorithm>
+#include <cassert>
 #include <cstddef>
 #include <cstdint>
+#include <functional>
+#include <type_traits>
 #include <utility>
 
-namespace pvq {
+namespace pvq::detail {
 
-#ifdef __clang__
+#if defined(__clang__)
     #define PVQ_UNROLL _Pragma("unroll")
-#else
+#elif defined(__GNUC__)
     #define PVQ_UNROLL _Pragma("GCC ivdep")
 #endif
 
@@ -42,6 +46,16 @@ void expand(auto&& f) {
     expand<0, End>(f);
 }
 
+inline void assume(bool pred) {
+    assert(pred);
+
+#if defined(__clang__)
+    __builtin_assume(pred);
+#elif defined(__GNUC__)
+    if (!pred) __builtin_unreachable();
+#endif
+}
+
 template<class T>
 concept SimdElement = std::is_arithmetic_v<T> && !std::is_const_v<T>;
 
@@ -51,6 +65,28 @@ using Avx = T __attribute__((vector_size(32)));
 template<SimdElement T>
 constexpr size_t AVX_LANES = 32 / sizeof(T);
 
+template<SimdElement T>
+Avx<T> loadu(T* ptr) {
+    if constexpr (std::is_same_v<T, float>) {
+        return _mm256_loadu_ps(ptr);
+    } else if constexpr (std::is_same_v<T, double>) {
+        return _mm256_loadu_pd(ptr);
+    } else {
+        return _mm256_loadu_si256((__m256i*)ptr);
+    }
+}
+
+template<SimdElement T>
+void storeu(T* ptr, Avx<T> x) {
+    if constexpr (std::is_same_v<T, float>) {
+        _mm256_storeu_ps(ptr, x);
+    } else if constexpr (std::is_same_v<T, double>) {
+        _mm256_storeu_pd(ptr, x);
+    } else {
+        _mm256_storeu_si256((__m256i*)ptr, x);
+    }
+}
+
 inline bool any(__m256i x) {
     return _mm256_movemask_epi8(x) != 0;
 }
@@ -59,4 +95,4 @@ inline bool all(__m256i x) {
     return _mm256_movemask_epi8(x) == -1;
 }
 
-}  // namespace pvq
+}  // namespace pvq::detail
